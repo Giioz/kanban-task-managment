157bc1e24915ee4f0a540ed43d71e299476a9125
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-11-30 17:12:22 +0400

    feat: interface for board

---
+++ src/interfaces/boardInterface.ts
@@ -0,0 +1,28 @@
+interface Subtask {
+    title: string;
+    isCompleted: boolean;
+  }
+  
+  interface Task {
+    title: string;
+    description: string;
+    status: string;
+    subtasks: Subtask[];
+  }
+  
+  interface Column {
+    name: string;
+    tasks: Task[];
+  }
+  
+  interface Board {
+    name: string;
+    isActive: boolean;
+    columns: Column[];
+  }
+  
+  interface Boards {
+    boards: Board[];
+  }
+
+export default Boards;
