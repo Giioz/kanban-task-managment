9a1ad19637f619d39a6672b5d6d319ed59d7eb13
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-12-01 14:35:34 +0400

    feat: params&state

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Header } from './components/Header'
+import { useState } from 'react'
 
 function App() {
-
+  const [boardModelOpen, setBoardModelOpen] = useState(false)
   return (
-    <Header />
+    <Header boardModelOpen={boardModelOpen} setBoardModelOpen={setBoardModelOpen}/>
   )
 }
 
