0614cff65bb497ca54c063da4f92203babc2b937
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-12-01 14:36:48 +0400

    feat: board modal&dynamic board name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,9 @@
 // states
 import { useState } from 'react'
 
+// interfaces
+import boardInterface from '../interfaces/boardInterface'
+
 // image svgs
 import iconDown from '../assets/dropdown.svg'
 import iconUp from '../assets/dropdown-up.svg'
@@ -9,9 +12,17 @@ import plus from '../assets/+.svg'
 import dots from '../assets/dots.svg'
 // components
 import { HeaderDropdown } from './HeaderDropdown'
+import { AddEditBoardModal } from '../modals/AddEditBoardModal'
+import { useDispatch, useSelector } from 'react-redux'
 
-export const Header = () => {
-    const [openDropdown, setDropdown] = useState(false);
+export const Header = ({boardModelOpen, setBoardModelOpen}:any) => {
+  const dispatch = useDispatch()
+
+  const [openDropdown, setDropdown] = useState(false);
+  const [boardType, setBoardType] = useState('add')
+
+  const boards = useSelector((state:boardInterface) => state.boards)
+  const board = boards.find( board => board.isActive)
   return (
     <div className='p-4 fixed left-0 right-0 bg-white dark:bg-[#2b2c37] z-50'>
         <header className='flex justify-between dark:text-white items-center'>
@@ -20,7 +31,7 @@ export const Header = () => {
                 <img className='h-6 w-6' src={logo} alt="" />
                 <h3 className='hidden md:inline-block font-bold font-sans md:text-4xl'>Kanban</h3>
                 <div className='flex items-center space-x-2'>
-                    <h3 className='max-w-[200px] md:text-2xl text-xl font-bold md:ml-20 font-sans'>Board name</h3>
+                    <h3 className='max-w-[200px] md:text-2xl text-xl font-bold md:ml-20 font-sans'>{board?.name}</h3>
                     <div className='h-4 flex items-center' onClick={() => setDropdown(state => !state)}>
                         <img className='md:hidden' src={openDropdown ? iconUp : iconDown} alt="" />
                     </div>
@@ -41,7 +52,11 @@ export const Header = () => {
 
         </header>
 
-        {openDropdown && <HeaderDropdown setDropdown={setDropdown}/>}
+        {openDropdown && <HeaderDropdown setDropdown={setDropdown} setBoardModelOpen={setBoardModelOpen}/>}
+        
+        {
+            boardModelOpen && <AddEditBoardModal type={boardType} setBoardModelOpen={setBoardModelOpen} setDropDown={setDropdown}/>
+        }
     </div>
 
   )
