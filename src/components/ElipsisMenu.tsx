2e0cb71c82699d41216c5d680e294c8fe0832736
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-12-08 17:57:24 +0400

    feat: elipsisMenu

---
+++ src/components/ElipsisMenu.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export const ElipsisMenu = ({type, setOpenEditModal, setOpenDeleteModal}:any) => {
+  return (
+     <div
+     className={ type === 'Boards' ? 'absolute top-16 right-6' : 
+        'absolute top-16 right-4'}
+     >
+
+        <div
+        className='flex justify-end items-center'
+        >
+
+            <div
+            className='w-40 text-sm z-50 font-medium shadow-md shadow-[#364e7e1a]
+            bg-white dark:bg-[#20212c] space-y-4 py-5 px-4 rounded-lg h-auto pr-12'
+            >
+                <p
+                onClick={() => {
+                    setOpenEditModal()
+                }}
+                className='cursor-pointer dark:text-gray-400 text-gray-700'
+                >
+                    Edit {type}
+                </p>
+                <p
+                className='cursor-pointer text-red-500'
+                >
+                    Delete {type}
+                </p>
+
+            </div>    
+
+        </div>
+
+     </div>
+  )
+}
